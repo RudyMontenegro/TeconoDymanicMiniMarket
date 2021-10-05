044a2597d10ced47f55ac4bdc077f99331b81bb6
alex-rodrigo-64 <alexcalle64@gmail.com>
2021-10-05 18:43:17 -0400

    casi a cago2.0

--- tecno-dynamic-mini-market/resources/views/home.blade.php
+++ tecno-dynamic-mini-market/resources/views/home.blade.php
@@ -8,21 +8,21 @@
                 <div class="card-header">Notificaciones</div>
                
                 <div class="card-body">
-                    <h1 class="text-center text-dark">Stock</h1>
+                    <h2 class="text-center text-dark">Stock</h2>
                    
                         @foreach ($stock as $stock)
-                            <h4 style="color: rgb(228, 105, 105)">Stock agotado tiene {{$stock->cantidad}} productos de {{$stock->nombre}}</h4>
+                            <h3 style="color: rgb(228, 105, 105)">!!!Stock agotado¡¡¡ tiene {{$stock->cantidad}} productos de {{$stock->nombre}}</h3>
                         @endforeach
-                <h1 class="text-center text-dark">Vencimiento Semanal</h1>
+                <h2 class="text-center text-dark">Vencimiento Semanal</h2>
                  
                 @foreach ($semana as $semana)
-                    <h4 style="color: rgb(55, 95, 32)">{{$semana->nombre}} vencera el {{$semana->fecha_vencimiento}}</h4>
+                    <h3 style="color: rgb(55, 95, 32)">{{$semana->nombre}} vencera el {{$semana->fecha_vencimiento}}</h3>
                 @endforeach
 
                     
-                <h1 class="text-center text-dark">Vencimiento Bimestral</h1>    
+                <h2 class="text-center text-dark">Vencimiento Bimestral</h2>    
                 @foreach ($mes as $mes)
-                    <h4 style="color: rgb(111, 105, 228)">{{$mes->nombre}} vencera el {{$mes->fecha_vencimiento}}</h4>
+                    <h3 style="color: rgb(111, 105, 228)">{{$mes->nombre}} vencera el {{$mes->fecha_vencimiento}}</h3>
                 @endforeach
                      
                 </div>
